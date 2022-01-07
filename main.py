8798c7119590eb3d404f8928795cb5d98cf9ff04
hpaterson97 <heather.paterson@sky.uk>
2022-01-07 12:05:08 +0000

    level design + creating tiles Co-authored-by: JulienMaheCrenn <JulienMaheCrenn@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 import sys
+from settings import *
+from level import Level
 
 # basic set up
 pygame.init()
-screen_width = 1200
-screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -14,6 +16,6 @@ while True:
             sys.exit()
 
     screen.fill('black')
-
+    level.run()
     pygame.display.update()
     clock.tick(60)
--- settings.py
+++ settings.py
@@ -1,11 +1,15 @@
 level_map = [
     'XX                                ',
+    'XX               XXX              ',
+    'XX                              XX',
+    'XX                       XXXXX    ',
+    'XX                     XXXXXXX    ',
     'XX                                ',
-    'XX                                ',
-    'XX                                ',
-    'XX                                ',
-    'XX                                ',
-    'XX                                ',
-    'XX                                ',
-    'XXXXXXXXXXXXXXXX                  ',
+    'XX           XXX  XX              ',
+    'XX         XXXXX  XXX             ',
+    'XXXXXXXXXXXXXXXX  XXXXX  XXXXX  XX',
 ]
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
