75c85e2b3245977353d5d85d77de083235a73498
Julien <julien.mahecrenn@sky.uk>
2022-01-07 15:34:17 +0000

    added dynamic animations to player sprite

--- player.py
+++ player.py
@@ -18,6 +18,9 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.8
         self.jump_speed = -16
 
+        # player status
+        self.status = 'idle'
+
     def import_character_assets(self):
         character_path = 'graphics/character/'
         self.animations = {'idle':[],'run':[],'jump':[],'fall':[]}
@@ -27,7 +30,7 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(full_path)
 
     def animate(self):
-        animation = self.animations['run']
+        animation = self.animations[self.status]
 
         # loop over the frame index
         self.frame_index += self.animation_speed
@@ -49,7 +52,15 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_SPACE]:
             self.jump()
 
-
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'fall'
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else: self.status = 'idle'
     
     def apply_gravity(self):
         self.direction.y += self.gravity
@@ -61,4 +72,5 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.get_status()
         self.animate()
